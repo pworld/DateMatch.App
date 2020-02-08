993bf223a71123441fbf82a0a6012c46c9e1296f
Gilang Prasetia <prameswara888@gmail.com>
2020-02-08 12:46:28 +0700

    Create Global Handling Error Request API

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using DateMatchApp.API.Helpers;
 
 namespace DateMatchApp.API
 {
@@ -56,7 +60,19 @@ namespace DateMatchApp.API
             }
             else
             {
-                app.UseHsts();
+                app.UseExceptionHandler(builder => {
+                    builder.Run(async context => {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if (error != null)
+                        {
+                            context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
+                // app.UseHsts();
             }
 
             // app.UseHttpsRedirection();
