40058ea812e65bc47796eebb0416c1a42e5e4980
Gilang Prasetia <prameswara888@gmail.com>
2020-02-15 19:19:56 +0700

    Fixing Auto mapper User detailed DTO

--- DTO/UserForDetailedDto.cs
+++ DTO/UserForDetailedDto.cs
@@ -7,7 +7,8 @@ namespace DateMatchApp.API.DTO
     public class UserForDetailedDto
     {
         public int Id { get; set; }
-        public string Username { get; set; }        public string Gender { get; set; }
+        public string Username { get; set; }        
+        public string Gender { get; set; }
         public int Age { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; }
--- Helpers/AutoMapperProfile.cs
+++ Helpers/AutoMapperProfile.cs
@@ -20,8 +20,10 @@ namespace DateMatchApp.API.Helpers
                     src => src.DateOfBirth.CalculateAge()));
             CreateMap<User, UserForDetailedDto>()
                 .ForMember(dest => dest.PhotoUrl, 
-                    opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url));
-            CreateMap<User, UserForDetailedDto>();
+                    opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, 
+                    opt => opt.MapFrom(
+                    src => src.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotosForDetailedDto>();
         }
     }
